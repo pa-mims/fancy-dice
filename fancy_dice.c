90a54c86e3548aeb4f14c82fff81e2ef7cf93462
pa-mims <mimsalex@gmail.com>
2021-07-06 18:15:49 -0400

    Make shake_dice() functional, add flip_coins()

--- fancy_dice.c
+++ fancy_dice.c
@@ -1,19 +1,23 @@
 #include "fancy_dice.h"
 #include <stdint.h>
+#include <curses.h>
 
-static uint32_t x = 0;                   // default value
 
+/* - THE SAUCE - */
+static uint32_t x = 0;                   // default PRNG seed value
+
+// You can play with these
 typedef uint32_t wheel32;
 static wheel32 c_wheel = 19937;         // must be a circular prime
 static wheel32 a_wheel = 11939;         // must be a circular prime
+static const int digits_in_a_wheel = 5;     // used in shake_dice()
+static const int digits_in_c_wheel = 5;     // used in shake_dice()
+static const uint32_t c_multiplier = 2401;  // any odd number
 
-static const uint32_t c_multiplier = 7 * 7 * 7 * 7;  // any odd number
-
-// Don't alter these formulas
+// But don't alter these formulas... unless you really want to
 static const uint32_t max_16bit_number = 65520;
 uint32_t a(void) { return (4 * a_wheel + 1); }
 uint32_t c(void) { return (c_multiplier * c_wheel); }
-
 
 uint32_t new_16bits(void) {
     x = a() * x + c();
@@ -22,18 +26,23 @@ uint32_t new_16bits(void) {
     return (x >> 16);
 }
 
-/*void rotate_wheel(static wheel32 *wheel) {
+void rotate_wheel(wheel32 *wheel) {
     *wheel = *wheel / 10 + 10000 * (*wheel % 10);
     return;
-}*/
+}
 
+
+/* - THE IMPLEMENTATION - */
 void seed_dice(int seed) {
     x = (uint32_t)seed;
     return;
 }
 
 void shake_dice(int num) {
-    new_16bits();
+    int a_rolls = roll(num, d4) % digits_in_a_wheel;
+    int c_rolls = roll(num, d4) % digits_in_c_wheel;
+    for ( ; a_rolls > 0; a_rolls--) rotate_wheel(&a_wheel);
+    for ( ; c_rolls > 0; c_rolls--) rotate_wheel(&c_wheel);
     return;
 }
 
@@ -59,3 +68,12 @@ int roll_percentile(void) {
     int value = roll(1, d10) * 10 + roll(1, d10);
     return (value ? value : 100); // 00 + 0 is 100 for percentile rolls
 }
+
+/*void test_wheels(void) {
+    for (int i = 0; i < 6; i++) {
+        rotate_wheel(&a_wheel);
+        rotate_wheel(&c_wheel);
+        printw("%du : %du\n", a_wheel, c_wheel);
+        getch();
+    }
+}*/
--- fancy_dice.h
+++ fancy_dice.h
@@ -3,8 +3,10 @@
 typedef enum { d4 = 4, d6 = 6, d8 = 8, d10 = 10, d12 = 12, d20 = 20 } dice_type;
 
 void seed_dice(int seed);           // seeds the RNG
-void shake_dice(int num);           // rolls rhe RNG num amount of times
+void shake_dice(int num);           // shakes up the RNG sauce
 int flip_coin(void);                // returns only 0 or 1
-int flip_coins(void);               // returns the number of heads
+int flip_coins(int);                // returns the number of heads
 int roll(int num, dice_type dice);  // rolls num amount of specified dice type
 int roll_percentile(void);          // rolls a percentile (2 d10) returns 1->100
+
+//void test_wheels(void); // test out the wheels
