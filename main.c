db2fcf9442fe0d953d8c7abf569333e1398bd281
pa-mims <mimsalex@gmail.com>
2021-07-06 18:16:50 -0400

    Remove <curses.h> from main.c example and use <stdio.h>

--- main.c
+++ main.c
@@ -1,15 +1,21 @@
-#include <curses.h>
+#include <stdio.h>
 #include "fancy_dice.h"
 
 int main(int argc, char *argv[]) {
-    initscr();
-    noecho();
-    while (getch() != 'q') {
-        clear();
-        printw("%d\n", flip_coin());
-        printw("%d\n", roll(1, d20));
-        printw("%d\n", roll_percentile());
-    }
-    endwin();
+    printf("\n\nPress <ENTER> to flip a coin, roll d20, and roll percentile.");
+    printf("\nEnter Q to exit.\n");
+
+    seed_dice(457773);
+    shake_dice(5);
+
+    char ch = '\0';
+    do {
+        printf("----------\n");
+        printf("   %d\n", flip_coin());
+        printf("   %d\n", roll(1, d20));
+        printf("   %d\n", roll_percentile());
+        ch = getchar();
+    } while (ch != 'q' && ch != 'Q');
+
     return 0;
 }
